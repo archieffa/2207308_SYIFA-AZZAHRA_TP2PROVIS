eeccb554da8ed24234275209bc77ce93c398e9a7
syifaazzhr <syifaazzhr26@gmail.com>
2024-03-28 23:49:34 +0700

    tp2

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,28 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // Profil Dokter images
+  static String imgFrame586 = '$imagePath/img_frame_586.svg';
+
+  static String imgMingcuteNotificationLine =
+      '$imagePath/img_mingcute_notification_line.svg';
+
+  static String imgVectorWhiteA700 = '$imagePath/img_vector_white_a700.svg';
+
+  static String imgImage9 = '$imagePath/img_image_9.png';
+
+  static String imgMaximize = '$imagePath/img_maximize.svg';
+
+  static String imgBag = '$imagePath/img_bag.svg';
+
+  static String imgVector = '$imagePath/img_vector.svg';
+
+  static String imgLinkedin = '$imagePath/img_linkedin.svg';
+
+  static String imgTelevision = '$imagePath/img_television.svg';
+
+  static String imgCall = '$imagePath/img_call.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
